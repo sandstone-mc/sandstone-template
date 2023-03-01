22f21da410ddceeb57d5a5183e69efd2cbe358bc
Mulverine <mulverin3@gmail.com>
2023-03-01 00:17:38 -0700

    Initial Commit

--- sandstone.config.ts
+++ sandstone.config.ts
@@ -1,12 +1,16 @@
-import type { SandstoneConfig } from 'sandstone'
+import type { DatapackConfig, SandstoneConfig } from 'sandstone'
 
 export default {
   name: 'template',
-  description: [ 'A ', { text: 'Sandstone', color: 'gold' }, ' data pack.' ],
-  formatVersion: 7,
+  packs: {
+    datapack: {
+      description: [ 'A ', { text: 'Sandstone', color: 'gold' }, ' data pack.' ],
+      packFormat: 11
+    } as DatapackConfig
+  },
   namespace: 'default',
   packUid: 'kZZpDK67',
-  saveOptions: { path: './.sandstone/output/datapack' },
+  saveOptions: {},
   onConflict: {
     default: 'warn',
   },
